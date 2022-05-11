7dfb7d40030cdd1a3db68ad34f23d6df4bf00b2f
Burble <56139877+BurbleH@users.noreply.github.com>
2022-05-11 18:40:29 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,114 @@
+var fer = false;
+if (!localStorage.getItem('s')) { localStorage.setItem('s', '[]'); }
+
+const st = JSON.parse(localStorage.getItem('s'));
+var stoper = true;
+const initoptions = {
+  inputStream: {
+    name: "Live",
+    type: "LiveStream",
+    target: document.querySelector('#quagga'),
+    constraints:{
+      facingMode: "environment",
+    }  // Or '#yourElement' (optional)
+  },
+  decoder: {
+    readers: ["ean_reader"],
+    debug: { drawBoundingBox: false }
+  }
+};
+function dostuff() {
+
+  var et = document.getElementById('booklist');
+  st.sort(function (a, b) {
+    let x = a.title.toUpperCase(),
+      y = b.title.toUpperCase();
+    return x == y ? 0 : x > y ? 1 : -1;
+
+  });
+  et.innerHTML = "";
+
+  console.log(st);
+  for (let i = 0; i < st.length; i++) {
+
+    let t = "<em>"+st[i].title+"</em> By: <em>"+st[i].author+"</em>";
+    let e = document.createElement("li");
+    let iger = document.createElement("img");
+    let delbut = document.createElement("button");
+    let brd = document.createElement("br");
+    delbut.innerHTML = "&times;";
+    delbut.addEventListener("click", function () {
+      st.splice(i, 1);
+      localStorage.setItem("s", JSON.stringify(st));
+      dostuff();
+    });
+
+    iger.src = st[i].cover;
+    iger.width = 150;
+    e.innerHTML = t;
+    e.appendChild(delbut);
+    e.appendChild(brd);
+    e.appendChild(iger);
+    et.appendChild(e);
+  }
+}
+
+let scabot = document.getElementById("scanbut");
+scabot.removeAttribute("disabled");
+scabot.onclick = function () {
+  console.log("starting");
+  document.getElementById("cameramodal").style.display = "block";
+  Quagga.init(initoptions, function (err) {
+    if (err) {
+      console.log(err);
+      return
+    }
+    console.log("Initialization finished. Ready to start");
+    Quagga.start();
+  });
+};
+Quagga.onDetected(async function (r) {
+  if (r.codeResult.code > 9780000000000 && r.codeResult.code < 9800000000000 && !fer) {
+    document.getElementById("cameramodal").style.display = "none";
+    Quagga.stop();
+    var x = await Book(r);
+    fer = true;
+    let img = document.getElementById("igerite");
+    img.src = x.cover;
+    document.getElementById("title").innerHTML = x.title;
+    document.getElementById("author").innerHTML = x.author;
+    document.getElementById("yesay").onclick = function () {
+      st.push(x);
+      let ser = JSON.stringify(st);
+      console.log(st);
+      console.log(ser);
+      localStorage.setItem('s', ser);
+      document.getElementById("myModal").style.display = "none";
+      dostuff();
+
+    };
+    document.getElementById("nosay").onclick = function () { document.getElementById("myModal").style.display = "none"; };
+    document.getElementById("myModal").style.display = "block";
+    console.log(x);
+
+
+    console.log("found");
+  } console.log(r.codeResult.code);
+});
+const Book = async (data) => {
+  self.isbn = data.codeResult.code;
+  let urlthing = "https://openlibrary.org/api/books?bibkeys=ISBN:" + self.isbn + "&jscmd=data&format=json";
+  console.log(urlthing);
+  let end = {};
+  let thing = await fetch(urlthing);
+  var obj = await thing.json();
+  obj = obj["ISBN:" + self.isbn + ""];
+  end.title = obj.title;
+  end.author = obj.authors[0].name;
+  end.cover = obj.cover.medium;
+  Promise.resolve(end);
+  return end;
+
+
+}
+dostuff();
